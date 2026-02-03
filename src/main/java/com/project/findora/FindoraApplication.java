ea198a83f59cfd5858fa04bcd2e99ba65b757c38
= <=>
2026-02-03 10:23:10 +0000

    first commit

---
+++ src/main/java/com/project/findora/FindoraApplication.java
@@ -0,0 +1,13 @@
+package com.project.findora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/findora/FindoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.findora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
